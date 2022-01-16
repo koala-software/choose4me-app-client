45c138a4a85f820ea1775968c5dc93b75ac3fb4f
frankev6 <frank.ev6@gmail.com>
2022-01-15 21:45:57 -0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable  } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Pressable style={styles.hostBtn}  onPress={()=> {}}><Text>HOST</Text></Pressable>
+      <Pressable style={styles.joinBtn}  onPress={()=> {}}><Text>JOIN</Text></Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#232735',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hostBtn:{
+      height:100,
+      width:100,
+      borderRadius:50,
+      backgroundColor:'#337AE2',
+      color:"#fff",
+      textAlign:'center',
+    },
+    joinBtn:{
+      height:100,
+      width:100,
+      borderRadius:50,
+      borderWidth:5,
+      backgroundColor:"transparent",
+      borderColor:'#337AE2',
+      color:"#fff",
+      textAlign:'center',
+    }
 });
